1f6846c6494fb692f5a73feffaa8312d10266652
HakimGh <hakimghaziali@gmail.com>
2022-08-31 00:35:18 +0430

    add some change in models

--- regression/admin.py
+++ regression/admin.py
@@ -3,8 +3,8 @@ from django.contrib import admin
 # Register your models here.
 
 
-from .models import Regres , parameters
+from .models import Regres , parameters , esm
 
 
 
-admin.register(Regres, parameters)(admin.ModelAdmin)
+admin.register(Regres, parameters,esm)(admin.ModelAdmin)
--- regression/models.py
+++ regression/models.py
@@ -1,7 +1,20 @@
+from unicodedata import name
 from django.db import models
 
 # Create your models here.
+class esm(models.Model):
 
+    name = models.CharField(max_length=80)
+
+    def __str__(self):
+        return self.name
+
+
+class parameters(models.Model):
+
+    coe = models.IntegerField()
+    intercept = models.IntegerField()
+    name = models.ForeignKey(esm , on_delete=models.CASCADE , related_name='esmesh')
 
 
 
@@ -9,12 +22,8 @@ class Regres(models.Model):
 
     x = models.PositiveIntegerField()
     y= models.PositiveIntegerField()
+    param = models.ForeignKey(parameters , on_delete=models.CASCADE , related_name='params')
+    name = models.ForeignKey(esm , on_delete=models.CASCADE , related_name='esme')
 
-
-
-class parameters(models.Model):
-
-    coe = models.IntegerField()
-    intercept = models.IntegerField()
-    regre = models.ForeignKey(Regres , on_delete=models.CASCADE , related_name='regression')
+    
 
