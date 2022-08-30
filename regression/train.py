e16faaa9fb7cd833bddd72dc8437e0750dc8ac85
HakimGh <hakimghaziali@gmail.com>
2022-08-30 11:28:52 +0430

    plot scatter in predictor

--- regression/train.py
+++ regression/train.py
@@ -18,10 +18,16 @@ def predictor(X,Y,Z):
     
     pred = model.predict(Z)
     fig, ax = plt.subplots( nrows=1, ncols=1 )
-    ax.plot(X, Y)
+    ax.scatter(X, Y)
+    ax.plot(X, model.predict(X))
     fig.savefig('static/images/to.png')
     plt.close(fig) 
 
+    Xd = pd.DataFrame(data = X)
+    Yd = pd.DataFrame(data = Y)
+    dt = pd.concat([Xd,Yd] , axis=1)
+
+    print(dt)
     return pred
 
 
